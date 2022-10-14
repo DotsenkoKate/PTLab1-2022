962e5e4ee514573cd80456609e303760e38b9e1e
DotsenkoKate <dotsenko.kate@yandex.ru>
2022-10-14 12:48:51 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -7,12 +7,14 @@ from CalcRating import CalcRating
 from TextDataReader import TextDataReader
 from JsonReader import JsonReader
 
+
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument("-p", dest="path", type=str, required=True,
                         help="Path to datafile")
     args = parser.parse_args(args)
     return args.path
+
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
@@ -30,6 +32,7 @@ def main():
 
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
+
 
 if __name__ == "__main__":
     main()
